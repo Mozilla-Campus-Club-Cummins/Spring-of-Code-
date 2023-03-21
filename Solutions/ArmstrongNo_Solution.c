4fc2f6bdafd1c8c5a04e67d7fc054bfb0a3793d1
Kruti Newalkar <newalkarkruti@gmail.com>
2023-03-21 22:49:11 +0530

    ArmstrongNo_Solution.c

--- Solutions/ArmstrongNo_Solution.c
+++ Solutions/ArmstrongNo_Solution.c
@@ -6,21 +6,26 @@
 # include <stdio.h>
 int main()
 {
-	int no, r, sum=0, cube, orig;
+	int no, r, sum=0, cube, orig;   //no accept i/p
+                                 //r - stores remainder i.e last digit
+                                //cube - stores cube of each digit
+                                //sum - adds up the cube of each digit
+                                //orig - stores the original i/p for checking
+
 	printf("Enter a whole no.: ");
 	scanf("%d", &no);
 	
-	orig = no;
+	orig = no;          //storing (taking a backup) of original i/p
 	
-	while(no != 0)
+	while(no != 0)      
 	{
-		r = no%10;
-		cube = r*r*r;
-		sum += cube;
-		no /= 10;
+		r = no%10;          //fetches last digit of no. for each iteration
+		cube = r*r*r;       //calc. cube of that digit
+		sum += cube;        //stores the sum of cubes
+		no /= 10;           //fetches rem (removes the last digit from R to L)
 	}
 	
-	if(sum == orig)
+	if(sum == orig)     //checks if original no. and sum of cubes is equal/not
 		printf("%d is an Armstrong No.", orig);
 	else
 		printf("No it's not an armstrong no.");
